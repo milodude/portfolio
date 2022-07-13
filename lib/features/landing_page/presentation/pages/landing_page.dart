efaf9cec78a2f2554ead376a9b71f5f621d46f59
MatiasR <milodude023@gmail.com>
2022-07-13 14:46:25 -0300

    [02] - Added missing references

--- lib/features/landing_page/presentation/pages/landing_page.dart
+++ lib/features/landing_page/presentation/pages/landing_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../widgets/experience.dart';
+import '../widgets/photograph.dart';
 import '../widgets/social_networks.dart';
 
 class LandingPage extends StatelessWidget {
@@ -17,56 +19,6 @@ class LandingPage extends StatelessWidget {
           PhotoGraph(heigth: heigth, width: width),
           SocialNetworks(heigth: heigth, width: width),
         ],
-      ),
-    );
-  }
-}
-
-class PhotoGraph extends StatelessWidget {
-  const PhotoGraph({
-    Key? key,
-    required this.heigth,
-    required this.width,
-  }) : super(key: key);
-
-  final double heigth;
-  final double width;
-
-  @override
-  Widget build(BuildContext context) {
-    return Positioned(
-      left: 0,
-      child: SizedBox(
-        height: heigth,
-        width: width * 0.5,
-        child: Image.asset(
-          'images/profile1.jpeg',
-          scale: 0.3,
-          fit: BoxFit.cover,
-        ),
-      ),
-    );
-  }
-}
-
-class Experience extends StatelessWidget {
-  const Experience({
-    Key? key,
-    required this.width,
-    required this.heigth,
-  }) : super(key: key);
-
-  final double width;
-  final double heigth;
-
-  @override
-  Widget build(BuildContext context) {
-    return Positioned(
-      left: width * 0.5,
-      child: SizedBox(
-        height: heigth,
-        width: width * 0.5,
-        child: const Placeholder(),
       ),
     );
   }
