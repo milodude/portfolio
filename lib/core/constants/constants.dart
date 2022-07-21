9dce6e94f4af7cf2e211f025f2e44da7f2224b2a
MatiasR <milodude023@gmail.com>
2022-07-21 09:49:40 -0300

    [22] - Updated images path in pubspec

--- lib/core/constants/constants.dart
+++ lib/core/constants/constants.dart
@@ -1,4 +1,2 @@
-const String linkedInImageUrl =
-    'https://cdn-icons-png.flaticon.com/512/174/174857.png';
 const String profilePath = 'images/profile4.jpeg';
 const String aboutMeImagePath = 'images/profile1.jpeg';
